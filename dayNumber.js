d02872ca593826864f5ab8fb2c6ebad4000f4e48
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-13 06:54:18 +0000

    add switch

--- dayNumber.js
+++ dayNumber.js
@@ -17,7 +17,32 @@ if (dayNumber === 0 ) {
     console.log('Invalid day number');
 }
 
+
+// switch
 let dayByNumber = 1;
 switch (dayByNumber) {
-    
+    case 0 :
+      console.log('Sunday');
+      break;
+    case 1 :
+      console.log('Monday');
+      break;
+    case 2 :
+      console.log('Tuesday');
+      break;
+    case 3 :
+      console.log('Wednesday');
+      break;
+    case 4 :
+      console.log('Thursday');
+     break;
+    case 5 :
+      console.log('Friday');
+      break;
+    case 6 :
+      console.log('Saturday');
+      break;
+    default:
+      console.log('Invalid day number');    
+      break;
 }
